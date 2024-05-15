72dd207152365593c19d1af476a80b766cfb4567
109 <109@109-14>
2024-05-15 11:01:01 +0200

    koszyk cd.

--- Ciosek_asp_net/Controllers/KoszykController.cs
+++ Ciosek_asp_net/Controllers/KoszykController.cs
@@ -22,10 +22,12 @@ namespace Ciosek_asp_net.Controllers
             if (koszyk != null)
             {
                 ViewBag.CalaSuma = koszyk.Sum(f => f.film.Cena * f.ilosc);
+                ViewBag.IloscBilet = koszyk.Sum(f => f.ilosc);
             }
             else
             {
                 ViewBag.CalaSuma = 0;
+                ViewBag.IloscBilet = 0;
             }
             return View(koszyk);
         }
@@ -33,6 +35,7 @@ namespace Ciosek_asp_net.Controllers
         [Route("Kup/{Id}")]
         public IActionResult Kup(int id)
         {
+            //System.Diagnostics.Debug.WriteLine("-------------");
             Film film = db.Filmy.Find(id);
 
             if (SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(HttpContext.Session, Consts.KluczSesji) == default(List<ElementKoszyka>))
@@ -43,26 +46,83 @@ namespace Ciosek_asp_net.Controllers
                     new ElementKoszyka{film=film, ilosc=1, wartosc=film.Cena}
                 };
                 SessionHelper.SetObjectAsJson(HttpContext.Session, Consts.KluczSesji, koszyk);
+                //System.Diagnostics.Debug.WriteLine("-------------A");
             }
             else
             {
                 // jest koszyk
                 List<ElementKoszyka> koszyk = SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(HttpContext.Session, Consts.KluczSesji);
-                int ind = koszyk.FindIndex(f => f.film == film);
+                int ind = koszyk.FindIndex(f => f.film.Id == film.Id);
+                //System.Diagnostics.Debug.WriteLine("-------------Index = " + ind);
                 if (ind >= 0)
                 {
                     //jest film w koszyku
                     koszyk[ind].ilosc += 1;
+                    //System.Diagnostics.Debug.WriteLine("-------------B");
                 }
                 else
                 {
                     //nie ma filmu w koszyku
                     koszyk.Add( new ElementKoszyka { film = film, ilosc = 1, wartosc = film.Cena });
+                    //System.Diagnostics.Debug.WriteLine("-------------C");
                 }
                 SessionHelper.SetObjectAsJson(HttpContext.Session, Consts.KluczSesji, koszyk);
             }
 
             return View();
+        }
+
+        [Route("Usun/{Id}")]
+        public IActionResult Usun(int id)
+        {
+            // założenie, że koszyk istnieje i film jest w koszyku
+            Film film = db.Filmy.Find(id);
+            List<ElementKoszyka> koszyk = SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(HttpContext.Session, Consts.KluczSesji);
+            int ind = koszyk.FindIndex(f => f.film.Id == film.Id);
+            if (koszyk[ind].ilosc > 1)
+            {
+                koszyk[ind].ilosc -= 1;
+            }
+            else
+            {
+                koszyk.RemoveAt(ind);
+            }
+            SessionHelper.SetObjectAsJson(HttpContext.Session, Consts.KluczSesji, koszyk);
+
+            return RedirectToAction("Index");
+        }
+
+        public int PobierzIloscRzeczyZKoszyka()
+        {
+            List<ElementKoszyka> koszyk = SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(HttpContext.Session, Consts.KluczSesji);
+
+            if (koszyk != null)
+            {
+                return koszyk.Sum(f => f.ilosc);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public int PobierzWartoscKoszyka()
+        {
+            List<ElementKoszyka> koszyk = SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(HttpContext.Session, Consts.KluczSesji);
+
+            if (koszyk != null)
+            {
+                return (int)koszyk.Sum(f => f.film.Cena * f.ilosc);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.CalaSuma = PobierzWartoscKoszyka();
+            ViewBag.IloscBilet = PobierzIloscRzeczyZKoszyka();
+            return await Task.FromResult((IViewComponentResult)View("_Menu", db.Kategorie.ToList()));
         }
     }
 }
---
+++ Ciosek_asp_net/Scripts/MenedzerKoszyka.cs
@@ -0,0 +1,48 @@
+﻿using Ciosek_asp_net.Helpers;
+using Ciosek_asp_net.Models;
+
+namespace Ciosek_asp_net.Nowy_folder
+{
+    public static class MenedzerKoszyka
+    {
+        public static int UsunZKoszyka(ISession session, int id)
+        {
+            var koszyk = (List<ElementKoszyka>)PobierzKoszyk(session);
+
+            var usuwanyFilm = koszyk.Find(f => f.film.Id == id);
+            int ilosc = 0;
+
+            if (usuwanyFilm == null)
+            {
+                return ilosc;
+            }
+
+            if(usuwanyFilm.ilosc > 1)
+            {
+                usuwanyFilm.ilosc--;
+                ilosc = usuwanyFilm.ilosc;
+            }
+            else
+            {
+                koszyk.Remove(usuwanyFilm);
+            }
+            
+            SessionHelper.SetObjectAsJson(session, Consts.KluczSesji, koszyk);
+            return ilosc;
+        }
+
+        private static object PobierzKoszyk(ISession session)
+        {
+            List<ElementKoszyka> koszyk = SessionHelper.GetObjectFromJson<List<ElementKoszyka>>(session, Consts.KluczSesji);
+
+            if (koszyk != null)
+            {
+                return koszyk;
+            }
+            else
+            {
+                return new List<ElementKoszyka>();
+            }
+        }
+    }
+}
